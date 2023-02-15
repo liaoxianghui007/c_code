7190844590d497c5aa545ea6a28da7dd8991620f
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-02-15 21:08:51 +0800

    Add files via upload

---
+++ 循环语句-2.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+
+int main()
+{
+	int ret = 0;
+	int ch = 0;
+	char password[20] = { 0 };
+	printf("请输入密码：");
+	scanf("%s", password);
+
+	//利用while循环，去除多余空格
+	while ((ch = getchar()) != '\n')
+	{
+		;
+	}
+
+	printf("请确认（Y/N)：");
+	ret = getchar();
+
+	if (ret == 'Y')
+		printf("确认成功");
+	else
+		printf("放弃确认");
+	
+	return 0;
+}
