34daa24ff52ef289def2491a76d700eb059e6b69
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-02-22 20:25:59 +0800

    Add files via upload

---
+++ 循环语句-for-3.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+
+int main()
+{
+	int x, y;
+
+	for (x = 0, y = 0; x < 2 && y < 5; x++, y++)
+	{
+		printf("liao xiang hui\n");
+	}
+
+
+	return 0;
+}
