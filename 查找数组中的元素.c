b402ac3850734d8585703676e421acc690daff88
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-02-27 22:05:16 +0800

    Add files via upload

---
+++ 查找数组中的元素.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13 };
+	int k;
+	scanf("%d", &k);//输入元素值
+
+	int sz = sizeof(arr) / sizeof(arr[0]);//计算元素个数
+	int left = 0;//左下标
+	int right = sz - 1;//右下标
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;//二分查找法
+
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d", mid);
+			break;
+		}
+	}
+	if (left > right)
+		printf("找不到了");
+
+	return 0;
+}
