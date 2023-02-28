8efece5a66b712f16309d32dfebf7da5a1fbfb34
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-02-28 21:33:09 +0800

    Add files via upload

---
+++ 登录操作.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入你的密码：");
+		scanf("%s", password);
+		if (strcmp(password, "123456") == 0)// == 不能用比较字符串，应该使用库函数strcmp
+		{
+			printf("登录成功\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误\n");
+		}
+	}
+	if (i == 3)
+		printf("错误3次，退出程序！");
+
+	return 0;
+}
