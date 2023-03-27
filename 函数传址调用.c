cec9032821c6a68fc8048b31f5cba8d8e7956f87
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-03-27 19:39:56 +0800

    Add files via upload

---
+++ 函数传址调用.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int Swap(int* pa ,int* pb)
+{
+	int temp = 0;
+	temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+}
+
+
+int main()
+{
+
+	int a = 20;
+	int b = 50;
+
+	Swap(&a, &b);
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+
+	return 0;
+}
