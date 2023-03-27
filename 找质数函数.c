23881e85104f51e401b793fd9f254152ca5d6aff
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-03-27 19:07:03 +0800

    Add files via upload

---
+++ 找质数函数.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int is_prime(int n)
+{
+	int j = 0;
+	for (j = 2; j < n; j++)
+	{
+		if (n % j == 0)
+			return 0;
+	}
+	return 1;
+}
+
+
+int main()
+{
+	//打印100-200之间的质数
+	int i = 0;
+	for ( i = 100; i < 200; i++)
+	{
+		if (is_prime(i) == 1)
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
