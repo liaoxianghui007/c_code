c2ab7117f5e03c898a734fe1ce5f81b092ec0f62
liaoxianghui007 <125141597+liaoxianghui007@users.noreply.github.com>
2023-03-27 19:48:56 +0800

    Add files via upload

---
+++ 函数传值调用.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+//函数的传址调用
+int Max(int x,int y)
+{
+	int z = x * y;
+	return z;
+}
+
+
+int main()
+{
+
+	int a = 20;
+	int b = 50;
+
+	int result = Max(a,b);
+	printf("result = %d\n", result);
+
+	return 0;
+}
